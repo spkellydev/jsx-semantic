342e9c2a50a54659486686d20761e794506a7263
Sean Kelly <skelly@aaski.com>
2018-12-14 11:47:14 -0500

    jsx semantic init

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'jsx-semantic'
+import { Section, Div, P, H1, H2, H3, H4, H5, H6 } from 'jsx-semantic'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Section useTagAsClassName={false}>
+          <Div>
+            <H1>JSX Semantic</H1>
+            <H2>Heading 2</H2>
+            <H3>Heading 3</H3>
+            <H4>Heading 4</H4>
+            <H5>Heading 5</H5>
+            <H6>Heading 6</H6>
+            <P>Paragraph</P>
+          </Div>
+        </Section>
       </div>
     )
   }
